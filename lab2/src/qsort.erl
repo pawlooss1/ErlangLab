26b987303faf61d8131b7e5c071c487200a9b1e7
pawlooss1 <pawlooss13@gmail.com>
2019-03-28 11:21:13 +0100

    Po drugich laboratoriach

--- lab2/src/qsort.erl
+++ lab2/src/qsort.erl
@@ -10,4 +10,19 @@
 -author("pawel").
 
 %% API
--export([]).
+-export([qs/1, randomElems/3, compareSpeeds/3]).
+
+lessThan(List, Arg) -> [X || X <- List, X < Arg].
+
+grtEqThan(List, Arg) -> [X || X <- List, X >= Arg].
+
+qs([]) -> [];
+qs([Pivot | Tail]) -> qs(lessThan(Tail, Pivot)) ++ [Pivot] ++ qs(grtEqThan(Tail, Pivot)).
+
+randomElems(N, Min, Max) ->
+  [rand:uniform(Max - Min + 1) + Min - 1 || A <- lists:seq(1,N)].
+
+compareSpeeds(List, Fun1, Fun2) ->
+  {Time1, _} = timer:tc(Fun1, [List]),
+  {Time2, _} = timer:tc(Fun2, [List]),
+  {Time1, Time2}.
