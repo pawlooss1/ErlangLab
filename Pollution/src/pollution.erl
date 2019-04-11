128c8e13c1ab2a8a03326ad46851247aff64eacb
pawlooss1 <pawlooss13@gmail.com>
2019-04-11 02:42:24 +0200

    Pollution gotowe, ale brzydkie

--- Pollution/src/pollution.erl
+++ Pollution/src/pollution.erl
@@ -10,7 +10,7 @@
 -author("pawel").
 
 %% API
--export([createMonitor/0, addStation/3, addValue/5, removeValue/4]).
+-export([createMonitor/0, addStation/3, addValue/5, removeValue/4, getOneValue/4, getStationMean/3, getDailyMean/3, getHourlyMean/4]).
 
 -record(station, {coordinates, name, measurements}).
 -record(measurement, {type, value, datetime}).
@@ -66,9 +66,7 @@ removeValueSecure([], _, _, Monitor) ->
 removeValueSecure([Key|_], DateTime, MeasureType, Monitor) ->
   #{Key := Station} = Monitor,
   #station{measurements = Measurements} = Station,
-  Pred = fun
-           (#measurement{type = Type, datetime = Date}) -> DateTime /= Date or MeasureType /= Type
-         end,
+  Pred = fun ({measurement, Type, _, Date}) -> ((DateTime /= Date) or (MeasureType /= Type)) end,
   UpdatedMeasurements = lists:filter(Pred, Measurements),
   Monitor#{Key := Station#station{measurements = UpdatedMeasurements}}.
 
@@ -78,3 +76,60 @@ removeValue(Coordinates, DateTime, MeasureType, Monitor) when is_tuple(Coordinat
 removeValue(Name, DateTime, MeasureType, Monitor) when is_list(Name) ->
   Key = getKey(Name, name, Monitor),
   removeValueSecure(Key, DateTime, MeasureType, Monitor).
+
+getValueSecure([], _, _, _) ->
+  "No measurement";
+getValueSecure([Key|_], DateTime, MeasureType, Monitor) ->
+  #{Key := Station} = Monitor,
+  #station{measurements = Measurements} = Station,
+  Pred = fun ({measurement, Type, _, Date}) -> ((DateTime == Date) and (MeasureType == Type)) end,
+  [ExtractedMeasurement] = lists:filter(Pred, Measurements),
+  #measurement{value = Value} = ExtractedMeasurement,
+  Value.
+
+getOneValue(Coordinates, DateTime, MeasureType, Monitor) when is_tuple(Coordinates) ->
+  Key = getKey(Coordinates, coordinates, Monitor),
+  getValueSecure(Key, DateTime, MeasureType, Monitor);
+getOneValue(Name, DateTime, MeasureType, Monitor) when is_list(Name) ->
+  Key = getKey(Name, name, Monitor),
+  getValueSecure(Key, DateTime, MeasureType, Monitor).
+
+getMeanSecure(Key, MeasureType, Monitor) ->
+  #{Key := Station} = Monitor,
+  #station{measurements = Measurements} = Station,
+  TypeMeasurements = lists:filter(fun (#measurement{type = Type}) -> Type == MeasureType end, Measurements),
+  calculateMean(TypeMeasurements).
+
+getStationMean(Coordinates, MeasureType, Monitor) when is_tuple(Coordinates) ->
+  Key = getKey(Coordinates, coordinates, Monitor),
+  getMeanSecure(Key, MeasureType, Monitor);
+getStationMean(Name, MeasureType, Monitor) when is_list(Name) ->
+  Key = getKey(Name, name, Monitor),
+  getMeanSecure(Key, MeasureType, Monitor).
+
+getDailyMean(Date, MeasureType, Monitor) ->
+  Stations = maps:values(Monitor),
+  Concatenator = fun (#station{measurements = Measurements}, List) -> List ++ Measurements end,
+  AllMeasurements = lists:foldl(Concatenator, [], Stations),
+  Measurements = lists:filter(fun(#measurement{type = Type, datetime = {D, _}}) -> ((Type == MeasureType) and (D == Date)) end, AllMeasurements),
+  calculateMean(Measurements).
+
+hourlyMeanSecure(Key, MeasureType, Time, Monitor) ->
+  #{Key := Station} = Monitor,
+  #station{measurements = Measurements} = Station,
+  FilteredMeasurements = lists:filter(fun(#measurement{type = Type, datetime = {_, T}}) -> ((Type == MeasureType) and (T == Time)) end, Measurements),
+  calculateMean(FilteredMeasurements).
+
+getHourlyMean(Coordinates, MeasureType, Time, Monitor) when is_tuple(Coordinates) ->
+  Key = getKey(Coordinates, coordinates, Monitor),
+  hourlyMeanSecure(Key, MeasureType, Time, Monitor);
+getHourlyMean(Name, MeasureType, Time, Monitor) when is_list(Name) ->
+  Key = getKey(Name, name, Monitor),
+  hourlyMeanSecure(Key, MeasureType, Time, Monitor).
+
+calculateMean(Measurements) ->
+  FSum = fun (#measurement{value = Val}, Acc) -> Acc + Val end,
+  FCount = fun (_, Acc) -> Acc + 1 end,
+  Sum = lists:foldl(FSum, 0, Measurements),
+  Count = lists:foldl(FCount, 0, Measurements),
+  Sum / Count.
