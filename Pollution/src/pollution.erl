2d87c70a783ec5d1e6f84652d4369597e0948523
pawlooss1 <pawlooss13@gmail.com>
2019-05-08 16:41:28 +0200

    Po zmianie struktury Monitor w pollution i refactoringu

--- Pollution/src/pollution.erl
+++ Pollution/src/pollution.erl
@@ -15,121 +15,111 @@
 -record(station, {coordinates, name, measurements}).
 -record(measurement, {type, value, datetime}).
 
-createMonitor() -> #{}.
+% Monitor : {#{Name => Station}, #{Coordinates => Name}}
+createMonitor() -> {#{}, #{}}.
+
+addStation(Name, Coordinates, Monitor) ->
+  {NameStation, CoordsName} = Monitor,
+  ContainsName = maps:is_key(Name, NameStation),
+  ContainsCoords = maps:is_key(Coordinates, CoordsName),
+  addStationSecure(Name, Coordinates, Monitor, ContainsName, ContainsCoords).
+
+addValue(Key, DateTime, MeasureType, Value, Monitor) ->
+  applySecureFunction(Key, DateTime, MeasureType, Value, Monitor, fun addValueSecure/6).
+
+removeValue(Key, DateTime, MeasureType, Monitor) ->
+  applySecureFunction(Key, DateTime, MeasureType, none, Monitor, fun removeValueSecure/6).
+
+getOneValue(Key, DateTime, MeasureType, Monitor) ->
+  applySecureFunction(Key, DateTime, MeasureType, none, Monitor, fun getValueSecure/6).
+
+getStationMean(Key, MeasureType, Monitor)->
+  applySecureFunction(Key, none, MeasureType, none, Monitor, fun getStationMeanSecure/6).
+
+getDailyMean(Date, MeasureType, Monitor) ->
+  applySecureFunction(none, Date, MeasureType, none, Monitor, fun getDailyMeanSecure/5).
+
+getHourlyMean(Key, Time, MeasureType, Monitor) ->
+  applySecureFunction(Key, Time, MeasureType, 0, Monitor, fun hourlyMeanSecure/6).
+
 
 addStationSecure(Name, Coordinates, Monitor, false, false) ->
-  maps:put({Name, Coordinates}, #station{coordinates = Coordinates, name = Name, measurements = []}, Monitor);
+  {NameStation, CoordsName} = Monitor,
+  Station = #station{coordinates = Coordinates, name = Name, measurements = []},
+  {NameStation#{Name => Station}, CoordsName#{Coordinates => Name}};
 addStationSecure(_, _, Monitor, _, _) ->
   Monitor.
 
-checkName(GivenName) ->
-  fun
-    ({Name, _}, A) -> (Name == GivenName) or A
+applySecureFunction(none, DateTime, MeasureType, Value, Monitor, Function) ->
+  {NameStation, CoordsName} = Monitor,
+  Function(DateTime, MeasureType, Value, NameStation, CoordsName);
+applySecureFunction(Coords, DateTime, MeasureType, Value, Monitor, Function) when is_tuple(Coords) ->
+  {NameStation, CoordsName} = Monitor,
+  GoodKey = maps:is_key(Coords, CoordsName),
+  if
+    GoodKey -> Function(maps:get(Coords, CoordsName), DateTime, MeasureType, Value, NameStation, CoordsName);
+    true -> noSuchStation
+  end;
+applySecureFunction(Name, DateTime, MeasureType, Value, Monitor, Function) when is_list(Name) ->
+  {NameStation, CoordsName} = Monitor,
+  GoodKey = maps:is_key(Name, NameStation),
+  if
+    GoodKey -> Function(Name, DateTime, MeasureType, Value, NameStation, CoordsName);
+    true -> noSuchStation
   end.
-checkCoords(GivenCoordinates) ->
-  fun
-    ({_, Coordinates}, A) -> (Coordinates == GivenCoordinates) or A
-  end.
 
-addStation(Name, Coordinates, Monitor) ->
-  Keys = maps:keys(Monitor),
-  ContainsName = lists:foldl(checkName(Name), false, Keys),
-  ContainsCoords = lists:foldl(checkCoords(Coordinates), false, Keys),
-  addStationSecure(Name, Coordinates, Monitor, ContainsName, ContainsCoords).
-
-getKey(Value, coordinates, Monitor) ->
-  Keys = maps:keys(Monitor),
-  lists:filter(fun ({_, Coords}) -> Coords == Value end, Keys);
-getKey(Value, name, Monitor) ->
-  Keys = maps:keys(Monitor),
-  lists:filter(fun ({Name, _}) -> Name == Value end, Keys).
-
-addValueSecure([], _, Monitor) ->
-  Monitor;
-addValueSecure([Key|_], Measurement, Monitor) ->
-  #{Key := Station} = Monitor,
+unpackStationAndMeasurements(Name, NameStation) ->
+  Station = maps:get(Name, NameStation),
   #station{measurements = Measurements} = Station,
+  {Station, Measurements}.
+
+addValueSecure(Name, DateTime, MeasureType, Value, NameStation, CoordsName) ->
+  {Station, Measurements} = unpackStationAndMeasurements(Name, NameStation),
+  Measurement = #measurement{type = MeasureType, value = Value, datetime = DateTime},
   UpdatedMeasurements = (Measurements -- [Measurement]) ++ [Measurement],
-  Monitor#{Key := Station#station{measurements = UpdatedMeasurements}}.
+  {NameStation#{Name := Station#station{measurements = UpdatedMeasurements}}, CoordsName}.
 
-addValue(Coordinates, DateTime, MeasureType, Value, Monitor) when is_tuple(Coordinates) ->
-  Measurement = #measurement{type = MeasureType, value = Value, datetime = DateTime},
-  Key = getKey(Coordinates, coordinates, Monitor),
-  addValueSecure(Key, Measurement, Monitor);
-addValue(Name, DateTime, MeasureType, Value, Monitor) when is_list(Name) ->
-  Measurement = #measurement{type = MeasureType, value = Value, datetime = DateTime},
-  Key = getKey(Name, name, Monitor),
-  addValueSecure(Key, Measurement, Monitor).
-
-removeValueSecure([], _, _, Monitor) ->
-  Monitor;
-removeValueSecure([Key|_], DateTime, MeasureType, Monitor) ->
-  #{Key := Station} = Monitor,
-  #station{measurements = Measurements} = Station,
-  Pred = fun ({measurement, Type, _, Date}) -> ((DateTime /= Date) or (MeasureType /= Type)) end,
+removeValueSecure(Name, DateTime, MeasureType, _, NameStation, CoordsName) ->
+  {Station, Measurements} = unpackStationAndMeasurements(Name, NameStation),
+  Pred = fun({measurement, Type, _, Date}) -> ((DateTime /= Date) or (MeasureType /= Type)) end,
   UpdatedMeasurements = lists:filter(Pred, Measurements),
-  Monitor#{Key := Station#station{measurements = UpdatedMeasurements}}.
+  {NameStation#{Name := Station#station{measurements = UpdatedMeasurements}}, CoordsName}.
 
-removeValue(Coordinates, DateTime, MeasureType, Monitor) when is_tuple(Coordinates) ->
-  Key = getKey(Coordinates, coordinates, Monitor),
-  removeValueSecure(Key, DateTime, MeasureType, Monitor);
-removeValue(Name, DateTime, MeasureType, Monitor) when is_list(Name) ->
-  Key = getKey(Name, name, Monitor),
-  removeValueSecure(Key, DateTime, MeasureType, Monitor).
+unpackValue([]) -> "No measurement";
+unpackValue([#measurement{value = Value}|_]) -> Value.
 
-getValueSecure([], _, _, _) ->
-  "No measurement";
-getValueSecure([Key|_], DateTime, MeasureType, Monitor) ->
-  #{Key := Station} = Monitor,
-  #station{measurements = Measurements} = Station,
-  Pred = fun ({measurement, Type, _, Date}) -> ((DateTime == Date) and (MeasureType == Type)) end,
-  [ExtractedMeasurement] = lists:filter(Pred, Measurements),
-  #measurement{value = Value} = ExtractedMeasurement,
-  Value.
+getValueSecure(Name, DateTime, MeasureType, _, NameStation, _) ->
+  {_, Measurements} = unpackStationAndMeasurements(Name, NameStation),
+  Pred = fun({measurement, Type, _, Date}) -> ((DateTime == Date) and (MeasureType == Type)) end,
+  ExtractedMeasurement = lists:filter(Pred, Measurements),
+  unpackValue(ExtractedMeasurement).
 
-getOneValue(Coordinates, DateTime, MeasureType, Monitor) when is_tuple(Coordinates) ->
-  Key = getKey(Coordinates, coordinates, Monitor),
-  getValueSecure(Key, DateTime, MeasureType, Monitor);
-getOneValue(Name, DateTime, MeasureType, Monitor) when is_list(Name) ->
-  Key = getKey(Name, name, Monitor),
-  getValueSecure(Key, DateTime, MeasureType, Monitor).
-
-getMeanSecure(Key, MeasureType, Monitor) ->
-  #{Key := Station} = Monitor,
-  #station{measurements = Measurements} = Station,
-  TypeMeasurements = lists:filter(fun (#measurement{type = Type}) -> Type == MeasureType end, Measurements),
+getStationMeanSecure(Name, _, MeasureType, _, NameStation, _) ->
+  {_, Measurements} = unpackStationAndMeasurements(Name, NameStation),
+  TypeMeasurements = lists:filter(fun(#measurement{type = Type}) -> Type == MeasureType end, Measurements),
   calculateMean(TypeMeasurements).
 
-getStationMean(Coordinates, MeasureType, Monitor) when is_tuple(Coordinates) ->
-  Key = getKey(Coordinates, coordinates, Monitor),
-  getMeanSecure(Key, MeasureType, Monitor);
-getStationMean(Name, MeasureType, Monitor) when is_list(Name) ->
-  Key = getKey(Name, name, Monitor),
-  getMeanSecure(Key, MeasureType, Monitor).
+mergeAllMeasurements(NameStation) ->
+  Stations = maps:values(NameStation),
+  Concatenator = fun(#station{measurements = Measurements}, List) -> List ++ Measurements end,
+  lists:foldl(Concatenator, [], Stations).
 
-getDailyMean(Date, MeasureType, Monitor) ->
-  Stations = maps:values(Monitor),
-  Concatenator = fun (#station{measurements = Measurements}, List) -> List ++ Measurements end,
-  AllMeasurements = lists:foldl(Concatenator, [], Stations),
-  Measurements = lists:filter(fun(#measurement{type = Type, datetime = {D, _}}) -> ((Type == MeasureType) and (D == Date)) end, AllMeasurements),
+getDailyMeanSecure(Date, MeasureType, _, NameStation, _) ->
+  AllMeasurements = mergeAllMeasurements(NameStation),
+  Pred = fun(#measurement{type = Type, datetime = {D, _}}) -> ((Type == MeasureType) and (D == Date)) end,
+  Measurements = lists:filter(Pred, AllMeasurements),
   calculateMean(Measurements).
 
-hourlyMeanSecure(Key, MeasureType, Time, Monitor) ->
-  #{Key := Station} = Monitor,
-  #station{measurements = Measurements} = Station,
-  FilteredMeasurements = lists:filter(fun(#measurement{type = Type, datetime = {_, T}}) -> ((Type == MeasureType) and (T == Time)) end, Measurements),
+hourlyMeanSecure(Name, Time, MeasureType, _, NameStation, _) ->
+  {_, Measurements} = unpackStationAndMeasurements(Name, NameStation),
+  Pred = fun(#measurement{type = Type, datetime = {_, T}}) -> ((Type == MeasureType) and (T == Time)) end,
+  FilteredMeasurements = lists:filter(Pred, Measurements),
   calculateMean(FilteredMeasurements).
 
-getHourlyMean(Coordinates, MeasureType, Time, Monitor) when is_tuple(Coordinates) ->
-  Key = getKey(Coordinates, coordinates, Monitor),
-  hourlyMeanSecure(Key, MeasureType, Time, Monitor);
-getHourlyMean(Name, MeasureType, Time, Monitor) when is_list(Name) ->
-  Key = getKey(Name, name, Monitor),
-  hourlyMeanSecure(Key, MeasureType, Time, Monitor).
-
+calculateMean([]) -> "No measurements";
 calculateMean(Measurements) ->
-  FSum = fun (#measurement{value = Val}, Acc) -> Acc + Val end,
-  FCount = fun (_, Acc) -> Acc + 1 end,
+  FSum = fun(#measurement{value = Val}, Acc) -> Acc + Val end,
+  FCount = fun(_, Acc) -> Acc + 1 end,
   Sum = lists:foldl(FSum, 0, Measurements),
   Count = lists:foldl(FCount, 0, Measurements),
   Sum / Count.
